13ee98361680090a47d1a9bdcc30e8559a9dde94
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2023-09-13 14:05:49 +0700

    Tutorial 2

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/',include('main.urls'))
+    path('',include('main.urls')),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
