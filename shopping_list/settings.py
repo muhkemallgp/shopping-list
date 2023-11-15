0ebbabb123aafff5b8a11c7c0b2ce58c6695e417
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2023-11-15 11:55:26 +0700

    edit concel

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 import os
-import corsheaders
+# import corsheaders
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
